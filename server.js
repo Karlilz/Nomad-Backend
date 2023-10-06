dc28976fbf2ad741f3c2a2a8c834ff34163c2f5d
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-10-06 16:17:42 -0400

    app use cors added to server file

--- server.js
+++ server.js
@@ -4,17 +4,16 @@ const app = express()
 const PORT = process.env.PORT || 4000;
 const morgan = require("morgan");
 const cors = require("cors");
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+// app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 const postController = require ('./controller/postController')
 const UserLogInController = require('./controller/UserLogInController');
 const cookieParser = require("cookie-parser");
 const mongoose = require('mongoose');
 
 
-
+app.use(cors());
 app.use(express.static("public"));
 // app.use(expressLayouts)
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("tiny"));
